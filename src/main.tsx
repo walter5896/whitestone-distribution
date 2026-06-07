d6b749933f678ab3ab9f6868763a67c0bd06e0aa
Walter Blundell <wbblundell333@gmail.com>
2026-06-06 19:27:50 -0600

    top scroll

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { ScrollToTop } from "./components/layout/ScrollToTop";
 
 import "./styles/variables.css";
 import "./styles/globals.css";
@@ -17,6 +18,7 @@ import "./styles/admin.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
